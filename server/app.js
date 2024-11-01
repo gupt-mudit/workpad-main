7bf5440bd008cd4de4ae547c43b7a604fe83aa0b
muditgupta <muditgupt01@gmail.com>
2024-11-01 11:53:39 +0530

    first commit

--- server/app.js
+++ server/app.js
@@ -84,4 +84,4 @@ app.get('/api/documents/:id', async (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
